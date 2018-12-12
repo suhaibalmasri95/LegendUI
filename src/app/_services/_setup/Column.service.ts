e8c11962d6dd80ecbff759b0d3695896963db98c
suhaibalmasri95 <suhaib.almasri95@hotmail.com>
2018-12-12 22:17:59 +0200

    fixes

--- src/app/_services/_setup/Column.service.ts
+++ src/app/_services/_setup/Column.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/catch';
-import { Column } from 'src/app/entities/Setup/Categories';
+import { Column } from '../../entities/Setup/Categories';
 
 @Injectable({
   providedIn: 'root'
